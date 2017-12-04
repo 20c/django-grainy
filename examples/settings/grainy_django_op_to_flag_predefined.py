c8c661af23db87e4dcf248c7d1abd17024cc49ac
Stefan Pratter <stefan@20c.com>
2017-12-04 10:28:17 +0000

    django_grainy_op_to_flag example

---
+++ examples/settings/grainy_django_op_to_flag_predefined.py
@@ -0,0 +1,6 @@
+GRAINY_DJANGO_OP_TO_FLAG = {
+    "add" : grainy.const.PERM_CREATE,
+    "delete" : grainy.const.PERM_DELETE,
+    "change" : grainy.const.PERM_UPDATE,
+    "view" : grainy.const.PERM_READ
+}
