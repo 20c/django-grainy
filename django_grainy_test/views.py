3b240f537df749dd5666e20ba53b1e87b5364cc8
Stefan Pratter <stefan@20c.com>
2017-12-06 11:37:34 +0000

    rest framework viewset test object

--- django_grainy_test/views.py
+++ django_grainy_test/views.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
+from django_grainy.decorators import grainy_rest_viewset
+
+from .models import ModelA
+from .serializers import ModelASerializer
 
 # Create your views here.
+
+@grainy_rest_viewset(
+    namespace = "api.a",
+    handlers = {
+        "api.a.*.nested_dict.secret" : { "explicit" : True }
+    }
+)
+class ModelAViewSet(viewsets.ModelViewSet):
+    queryset = ModelA.objects.all()
+    serializer_class = ModelASerializer
