29e918f646081f428b38f2214962ebafb5c0bb9f
Stefan Pratter <stefan@20c.com>
2017-12-06 11:36:36 +0000

    admin forms for test model a

--- django_grainy_test/admin.py
+++ django_grainy_test/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import ModelA, ModelB
+
 # Register your models here.
+
+@admin.register(ModelA)
+class ModelAAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
