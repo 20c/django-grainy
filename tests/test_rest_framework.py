875fe646169af3f856da4a079c3fa4746b768fa4
Stefan Pratter <stefan@20c.com>
2017-12-08 15:05:58 +0000

    test permission gate on grainy_rest_viewset

--- tests/test_rest_framework.py
+++ tests/test_rest_framework.py
@@ -3,7 +3,10 @@ from django.test import RequestFactory
 from .util import UserTestCase
 
 from grainy.const import (
-    PERM_READ
+    PERM_READ,
+    PERM_CREATE,
+    PERM_UPDATE,
+    PERM_DELETE
 )
 
 from grainy.core import (
@@ -26,7 +29,7 @@ class TestGrainyViewSet(UserTestCase):
 
         cls.users["user_a"].grainy_permissions.add_permission_set(
             PermissionSet({
-                "api" : PERM_READ,
+                "api" : PERM_READ | PERM_CREATE | PERM_UPDATE | PERM_DELETE,
                 "api.a.*.nested_dict.secret": PERM_READ
             })
         )
@@ -38,6 +41,33 @@ class TestGrainyViewSet(UserTestCase):
         )
 
         ModelA.objects.create(name="Test1")
+
+
+    def test_grainy_viewset_gate(self):
+
+        client_a = APIClient()
+        client_a.force_authenticate(user=self.users["user_a"])
+
+        r = client_a.post("/a/", {"name":"bla"})
+        self.assertEqual(r.status_code, 201)
+        r = client_a.put("/a/2/", {"name":"bla2"})
+        self.assertEqual(r.status_code, 200)
+        r = client_a.patch("/a/2/", {"name":"bla3"})
+        self.assertEqual(r.status_code, 200)
+        r = client_a.delete("/a/2/", follow=True)
+        self.assertEqual(r.status_code, 404)
+
+        client_b = APIClient()
+        client_b.force_authenticate(user=self.users["user_b"])
+
+        r = client_b.post("/a/", {"name":"bla"})
+        self.assertEqual(r.status_code, 403)
+        r = client_b.put("/a/1/", {"name":"bla2"})
+        self.assertEqual(r.status_code, 403)
+        r = client_b.patch("/a/1/", {"name":"bla3"})
+        self.assertEqual(r.status_code, 403)
+        r = client_b.delete("/a/1/", follow=True)
+        self.assertEqual(r.status_code, 403)
 
 
 
