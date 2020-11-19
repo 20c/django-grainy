697796b4d9ba19092f3b9af4e4d05d4517ea55cd
Stefan Pratter <stefan@20c.com>
2020-11-19 13:52:42 +0000

    add PermissionDenied

--- src/django_grainy/exceptions.py
+++ src/django_grainy/exceptions.py
@@ -2,3 +2,7 @@ class DecoratorRequiresNamespace(ValueError):
     def __init__(self, decorator):
         super().__init__("This decorator requires you to specify a namespace")
         self.decorator = decorator
+
+class PermissionDenied(Exception):
+    def __init__(self, reason):
+        super(PermissionDenied, self).__init__(f"Permission denied: {reason}")
