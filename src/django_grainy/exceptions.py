a03b6b53f43c2bdad5176ad75a3b6acc4dda4154
Jason Carvalho <jason@20c.com>
2022-07-27 17:18:08 +0530

    add type hints

--- src/django_grainy/exceptions.py
+++ src/django_grainy/exceptions.py
@@ -1,10 +1,11 @@
-# TODO: typeing hints here
+from typing import Any, Callable
+
 class DecoratorRequiresNamespace(ValueError):
-    def __init__(self, decorator):
+    def __init__(self, decorator: Callable):
         super().__init__("This decorator requires you to specify a namespace")
         self.decorator = decorator
 
 
 class PermissionDenied(Exception):
-    def __init__(self, reason):
+    def __init__(self, reason: Any):
         super().__init__(f"Permission denied: {reason}")
